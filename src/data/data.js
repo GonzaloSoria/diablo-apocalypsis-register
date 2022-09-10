b9e8a583af254deb98c45555edff604c0141a294
GonzaloSoria <gonzalo.sooria@gmail.com>
2022-09-09 22:15:55 -0300

    updateing

--- src/data/data.js
+++ src/data/data.js
@@ -1,7 +1,7 @@
 export const data =  {
     hero: {
         title: 'Preparate para la batalla',
-        subtitle: 'Salva al mundo del infierno',
+        subtitle: 'Salva al mundo del apocalypsis',
         copy: 'Registrate y esperá el llamado de la Luz',
         ctaLink: 'https://youtu.be/DT9SSA4lKLA',
         ctaCopy: 'Registrarse'
@@ -14,44 +14,44 @@ export const data =  {
     characters: {
         title: 'Encontrá tu Héroe',
         copy: 'Limpia cada sector de las ordas del infierno para ganar mayor luz',
-        heroes: [
-            {
-                image: './assets/img/barbarian.png',
-                name: 'Bárbaro',
-                description: 'Los guia un deber ancestral en la montaña sagrada.',
-                item: './assets/img/barbarian-item.png'
-            },
-            {
-                image: './assets/img/paladin.png',
-                name: 'Paladin',
-                description: 'Los paladines son campeones indomables de la fe y la ley.',
-                item: './assets/img/paladin-item.png'
-            },
-            {
-                image: './assets/img/hechicera.png',
-                name: 'Hechicera',
-                description: 'Un conducto viviente de poder arcano.',
-                item: './assets/img/hechicera-item.png'
-            },
-            {
-                image: './assets/img/nigromante.png',
-                name: 'Nigromante',
-                description: 'Líderes implacables y maestros de la vida y la muerte',
-                item: './assets/img/nigromante-item.png'
-            },
-            {
-                image: './assets/img/monje.png',
-                name: 'Monje',
-                description: 'El emblema que llevan los distingue como supervivientes',
-                item: './assets/img/monje-item.png'
-            },
-            {
-                image: './assets/img/cazador.png',
-                name: 'Cazador',
-                description: 'Implacables luchadores que atacan desde lejos',
-                item: './assets/img/cazador-item.png'
-            },
-        ]
+        // heroes: [
+        //     {
+        //         image: './assets/img/barbarian.png',
+        //         name: 'Bárbaro',
+        //         description: 'Los guia un deber ancestral en la montaña sagrada.',
+        //         item: './assets/img/barbarian-item.png'
+        //     },
+        //     {
+        //         image: './assets/img/paladin.png',
+        //         name: 'Paladin',
+        //         description: 'Los paladines son campeones indomables de la fe y la ley.',
+        //         item: './assets/img/paladin-item.png'
+        //     },
+        //     {
+        //         image: './assets/img/hechicera.png',
+        //         name: 'Hechicera',
+        //         description: 'Un conducto viviente de poder arcano.',
+        //         item: './assets/img/hechicera-item.png'
+        //     },
+        //     {
+        //         image: './assets/img/nigromante.png',
+        //         name: 'Nigromante',
+        //         description: 'Líderes implacables y maestros de la vida y la muerte',
+        //         item: './assets/img/nigromante-item.png'
+        //     },
+        //     {
+        //         image: './assets/img/monje.png',
+        //         name: 'Monje',
+        //         description: 'El emblema que llevan los distingue como supervivientes',
+        //         item: './assets/img/monje-item.png'
+        //     },
+        //     {
+        //         image: './assets/img/cazador.png',
+        //         name: 'Cazador',
+        //         description: 'Implacables luchadores que atacan desde lejos',
+        //         item: './assets/img/cazador-item.png'
+        //     },
+        // ]
     },
     called: {
         title: 'Enfrenta tu destino',
